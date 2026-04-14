ac6abf5a1490103f3e25d7a5548cd49301809d57
lorenzo.ghiro <lorenzoghiro@gmail.com>
2026-04-14 21:30:10 +0200

    removed fade-in and other slow animations

--- assets/js/site.js
+++ assets/js/site.js
@@ -142,31 +142,6 @@
     });
   });
 
-  // ----- Fade-in on Scroll -----
-
-  var fadeElements = document.querySelectorAll('.fade-in-section');
-  if (fadeElements.length > 0 && 'IntersectionObserver' in window) {
-    var observer = new IntersectionObserver(function (entries) {
-      entries.forEach(function (entry) {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('visible');
-          observer.unobserve(entry.target);
-        }
-      });
-    }, {
-      threshold: 0.1,
-      rootMargin: '0px 0px -40px 0px'
-    });
-
-    fadeElements.forEach(function (el) {
-      observer.observe(el);
-    });
-  } else {
-    fadeElements.forEach(function (el) {
-      el.classList.add('visible');
-    });
-  }
-
   // ----- Site Search -----
 
   var searchToggleBtn = document.getElementById('searchToggle');
